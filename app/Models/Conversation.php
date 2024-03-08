7540a1ecedfc1aa1b944b5470bdbe27efffe4b8c
Ephraim Basubi <basubiephrem@gmail.com>
2024-03-08 12:35:26 +0300

    Load conversation of the user

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -17,4 +17,12 @@ class Conversation extends Model
     public function messages(){
         return $this->hasMany(Message::class);
     }
+
+    public function getReceiver() {
+        if($this->sender_id === auth()->id()) {
+            return User::firstWhere('id', $this->receiver_id);
+        }else{
+            return User::firstWhere('id', $this->sender_id);
+        }
+    }
 }

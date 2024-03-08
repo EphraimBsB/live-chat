49cf067b43cda8c40f38c2443dd82b1329c1d630
Ephraim Basubi <basubiephrem@gmail.com>
2024-03-08 10:03:03 +0300

    chat models and user model with relationship

--- database/migrations/2024_03_08_063338_create_messages_table.php
+++ database/migrations/2024_03_08_063338_create_messages_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('conversation_id')->constrained();
+
+            $table->unsignedBigInteger('sender_id');
+            $table->foreign('sender_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('receiver_id')->references('id')->on('users');
+
+            $table->timestamp('read_at')->nullable();
+
+            // delete actions
+            $table->timestamp('receiver_deleted_at')->nullable();
+            $table->timestamp('sender_deleted_at')->nullable();
+
             $table->timestamps();
         });
     }
